3fa269f1bb073a2ee0838f724a6afe839b06a7e1
Kethlyn <kethlynsaraa@gmail.com>
2023-01-19 14:18:05 -0300

    feat: delete car

--- src/main/java/com/crudlover/api/controller/CarController.java
+++ src/main/java/com/crudlover/api/controller/CarController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -44,5 +45,10 @@ public class CarController {
             car.setValor(req.valor());
             return repository.save(car);
         });
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        repository.deleteById(id);
     }
 }
